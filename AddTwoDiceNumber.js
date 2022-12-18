036ce1825b608e04c1b8d18b6fd9f12d5eefcb9f
Kajalwankhede <kajuwankhede1510@gmail.com>
2022-12-18 20:22:46 +0530

    Add two random dice number and print the result

---
+++ AddTwoDiceNumber.js
@@ -0,0 +1,9 @@
+//3. Use REPL – Add two Random Dice Number and Print the Result
+
+
+let dice1=Math.floor(Math.random() *6) +1;
+console.log(dice1);
+let dice2=Math.floor(Math.random() *6) +1;
+console.log(dice2);
+let result=dice1+dice2;
+console.log("Add Two Dice Number: "+result);
