0da88cef18c980ffe3cc504535c2cbf1d182f4bc
Kajalwankhede <kajuwankhede1510@gmail.com>
2022-12-18 22:28:12 +0530

    program that takes a input and determines if the number is a prime.

---
+++ PrimeNumber.js
@@ -0,0 +1,20 @@
+//3. Write a program that takes a input and determines if the number is a prime.
+
+let number = 11
+for (let i = 0 ; i <= number ; i++) 
+{
+    let numNotPrime = 0
+    for (let count = 2; count <number ; count++) {
+        if (number % count  == 0) {
+            numNotPrime++
+            break;
+        }
+    }
+    if (numNotPrime) {
+        console.log(number +" is not prime");
+    } else {
+        console.log(number +" is prime");
+    }
+   
+}
+
